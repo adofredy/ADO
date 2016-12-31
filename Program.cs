12feb68ad3222b44b874375e7b1a1280f3661774
adofredy <1320818@utp.edu.pe>
2016-12-31 15:00:03 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Task; 
+
+namespace presentacion6
+{ 
+  class Program
+  { 
+      static void Main(string() args)
+      {
+      ClientesVentas Cliente;
+      Cliente.IdCliente = 123;
+      Cliente.Apellidos = "Perez Piyo";
+      Cliente.Nombres ="Juanito";
+      Cliente.RFC="HEM";
+      Cliente.Direccion="HEM";
+      Cliente.Colonia="la fe de C";
+      Cliente.Municipio="Chepqew";
+      Console.WriteLine(Cliente.Apellidos + " " + Cliente.Nombres);
+      Console.WriteLine(Cliente.Direccion + " " + Cliente.Colonia+" "+ ClienteMunicipio);
+      Console.WriteLine(Cliente.RFC);
+      if (Cliente.EsCredito)
+      
+      }
+  }
+  
+} 
