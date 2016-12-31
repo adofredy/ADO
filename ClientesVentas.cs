f177e25fff63f1551f7f29254ecb8b4f21ec572f
adofredy <1320818@utp.edu.pe>
2016-12-31 14:41:26 -0500

    Create ClientesVentas.cs

---
+++ ClientesVentas.cs
@@ -0,0 +1,37 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Task; 
+
+namespace presentacion1 
+{ 
+  public class Clientes
+
+    { 
+      private int String RFC;
+      {
+        get {return _RFC ;}
+        set {_RFC = value;}
+      }
+    
+      private string Direccion;
+      {
+        get {return _Direccion;}
+        set {_Direccion = value;}
+      }
+      
+      private string _Colonia;
+      {
+        get {return Colonia;}
+        set {_Colonia = value;}
+      }
+      
+      private string _Municipio;
+      {
+        get {return Municipio;}
+        set {_Municipio = value;}
+      }
+    
+    }
+  
+} 
